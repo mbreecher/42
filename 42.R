0376b4cdf7f761664e977ff7d605590a5c758217
mike breecher <michaelbreecher@gmail.com>
2014-12-20 22:44:32 -0700

    added code to populate historical billable and project hours by service level

--- 42.R
+++ 42.R
@@ -7,15 +7,36 @@ source("import_functions.r")
 source("transformations.r")
 
 #////////////////////////////////
-# Billable Hours by service level
+# Billable Hours by xbrl status
 #////////////////////////////////
 
-collapsed_time <- collapsed_time()
+setwd("C:/R/workspace/shared")
+source("monthly_time.R")
 
+collapsed_weekly <- timelog_with_status()
+billable <- aggregate(Hours ~ monthyear +  xbrl_status + Billable + form_type, data = collapsed_weekly, FUN = sum)
+billable <- billable[billable$Billable %in% 1,]
+billable <- billable[order(billable$monthyear),] #sort
+agg_billable <- aggregate(Hours ~ monthyear + xbrl_status + form_type, data = billable, FUN = sum) #aggregate by type
 
+#cast wide to prepare for rbind
+billable_hours <- dcast(agg_billable, xbrl_status + form_type ~ monthyear, sum, value.var = "Hours")
 
+#////////////////////////////////
+# Flat Fee Hours by service level
+#////////////////////////////////
 
+# setwd("C:/R/workspace/shared")
+# source("monthly_time.R")
+# 
+# collapsed_weekly <- timelog_with_status()
+project_time <- aggregate(Hours ~ monthyear +  Service.Type + Billable + Form.Type, data = collapsed_weekly, FUN = sum)
+project_time <- project_time[project_time$Billable %in% 0,]
+project_time <- project_time[order(project_time$monthyear),] #sort
+agg_project <- aggregate(Hours ~ monthyear + Service.Type + Form.Type, data = project_time, FUN = sum) #aggregate by type
 
+#cast wide to prepare for rbind
+project_hours <- dcast(agg_project, Service.Type + Form.Type ~ monthyear, sum, value.var = "Hours")
 
 #////////////////////////////////
 # scheduled services by month
@@ -30,7 +51,5 @@ scheduled_by_month$Service.Name <- paste(scheduled_by_month$Form.Type, scheduled
 approved_groups <- c("10-K Standard Import","10-Q Standard Import","10-K Full Service Standard Import","10-Q Full Service Standard Import","10-K Basic Maintentance","10-Q Basic Maintentance","K-K Roll Forward","Q-K Roll Forward","Q-Q Roll Forward","K-Q Roll Forward","10-K Full Service Roll Forward","10-Q Full Service Roll Forward")
 scheduled_by_month[!scheduled_by_month$Service.Name %in% approved_groups,]$Service.Name <- "Other Services"
 scheduled_by_month <- scheduled_by_month[order(scheduled_by_month$monthyear),]
-#cast wide to setup rbind
+#cast wide to prepare for rbind
 scheduled_services <- dcast(scheduled_by_month, Service.Name ~ monthyear, sum, value.var = "Services.ID")
-
-model_time_ps <- dcast(agg_time_model, service_type + form ~ relative_week, sum, value.var = "psm_time")
