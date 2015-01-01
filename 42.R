0baea1710cd580950eea69ade25cc79f6c27a06f
mike breecher <michaelbreecher@gmail.com>
2015-01-01 07:48:42 -0700

    added detail tagging projects to names list for scheduled projects and removed migrations from dataframe

--- 42.R
+++ 42.R
@@ -65,8 +65,9 @@ services <- services[services$filing.estimate >= "2013-06-30",]
 services$monthyear <- format(services$filing.estimate, format = "%y-%m")
 
 scheduled_by_month <- aggregate(Services.ID ~ monthyear + Service.Type + Form.Type, data = services, FUN = length)
+scheduled_by_month <- scheduled_by_month[!scheduled_by_month$Service.Type %in% c("Migration"),]
 scheduled_by_month$Service.Name <- paste(scheduled_by_month$Form.Type, scheduled_by_month$Service.Type, sep = " ")
-name_order <- c("10-K Standard Import","10-Q Standard Import","10-K Full Service Standard Import","10-Q Full Service Standard Import","10-K Basic Maintentance","10-Q Basic Maintentance","K-K Roll Forward","Q-K Roll Forward","Q-Q Roll Forward","K-Q Roll Forward","10-K Full Service Roll Forward","10-Q Full Service Roll Forward")
+name_order <- c("10-K Detail Tagging","10-Q Detail Tagging","10-K Full Review","10-Q Full Review","10-K Standard Import","10-Q Standard Import","10-K Full Service Standard Import","10-Q Full Service Standard Import","10-K Maintenance","10-Q Maintenance","K-K Roll Forward","Q-K Roll Forward","Q-Q Roll Forward","K-Q Roll Forward","10-K Full Service Roll Forward","10-Q Full Service Roll Forward")
 scheduled_by_month[!scheduled_by_month$Service.Name %in% name_order,]$Service.Name <- "Other Services"
 scheduled_by_month <- scheduled_by_month[order(scheduled_by_month$monthyear),]
 #cast wide to prepare for rbind
